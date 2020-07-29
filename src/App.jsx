ee7b07336b16c91b36387166e9f2f94cab9e660a
Sima <sima.boreiri@gmail.com>
2020-07-29 15:46:31 +0200

    writes code to work on state and change the state

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  state = {
+    greeting: "Hi",
+    name: "World"
+  }
+  onGreetingHandler = () => {
+    this.setState({greeting: "Good Morning"})
+  }
+  onNameHandler = () => {
+    this.setState({name: "Juniors"})
+  }
+
   render() {
     return (
-      <div> Hello World </div>
+      <div>
+         <div>{this.state.greeting}{this.state.name} </div>
+         <button onClick={this.onGreetingHandler}>Click me first!</button>
+         <button onClick={this.onNameHandler}>Click me then!</button>
+      </div>
     );
   }
 }
